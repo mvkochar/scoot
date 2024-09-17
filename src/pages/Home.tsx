f74c859ad1aa296afa48c97141b9cb870ef02038
sgkochar <sgkochar@gmail.com>
2024-09-17 14:08:27 +0300

    add About blocks to Home page, create Footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,11 +48,59 @@ const Home = () => {
             <div><img src="/images/features3.svg" alt="features3" /></div>
             <h3 className="features-item-title">Enjoy the ride</h3>
             <p className="features-item-desc">
-              Scan the QR code and the bike will unlock. Retract the cable lock, 
-              put on a helmet, and you're off! Always lock bikes away from walkways 
+              Scan the QR code and the bike will unlock. Retract the cable lock,
+              put on a helmet, and you're off! Always lock bikes away from walkways
               and accessibility ramps.
             </p>
           </div>
+        </div>
+      </section>
+      <section className='home-about home-about-first d-f align-center'>
+        <div>
+          <h2 className="home-about-title">Easy to use riding telemetry</h2>
+          <p className="home-about-desc">
+            The Scoot app is available with riding telemetry. This means it can show
+            you your average speed, how long you've been using the scooter, your traveling
+            distance, and many more things all in an easy to use app.
+          </p>
+          <CustomButton>Learn More</CustomButton>
+        </div>
+        <div className="pos-r">
+          <div><img src="/images/home-about1.png" alt="home-about1" /></div>
+          <div className="home-about-mask"><img src="/images/blue-mask-right.png" alt="blue-mask-right" /></div>
+          <div className="home-about-path"><img src="/images/path3.svg" alt="path3" /></div>
+        </div>
+      </section>
+      <section className='home-about home-about-second d-f align-center'>
+        <div className="pos-r">
+          <div><img src="/images/home-about2.png" alt="home-about2" /></div>
+          <div className="home-about-mask"><img src="/images/blue-mask-left.png" alt="blue-mask-left" /></div>
+          <div className="home-about-path"><img src="/images/path4.svg" alt="path3" /></div>
+        </div>
+        <div>
+          <h2 className="home-about-title">Coming to a city near you</h2>
+          <p className="home-about-desc">
+            Scoot is available in 4 major cities so far. We're expanding rapidly, so be sure
+            to let us know if you want to see us in your hometown. We're aiming to let our
+            scooters loose on 23 cities over the coming year.
+          </p>
+          <CustomButton>Learn More</CustomButton>
+        </div>
+      </section>
+      <section className='home-about home-about-third d-f align-center'>
+        <div>
+          <h2 className="home-about-title">Zero hassle payments</h2>
+          <p className="home-about-desc">
+            Our payment is as easy as one two three. We accept most credit cards and debit cards. 
+            You can also link your PayPal account inside the app. Need to pay later? No worries! 
+            You can defer payment for up to a month.
+          </p>
+          <CustomButton>Learn More</CustomButton>
+        </div>
+        <div className="pos-r">
+          <div><img src="/images/home-about3.png" alt="home-about3" /></div>
+          <div className="home-about-mask"><img src="/images/blue-mask-right.png" alt="blue-mask-right" /></div>
+          <div className="home-about-path"><img src="/images/path5.svg" alt="path5" /></div>
         </div>
       </section>
     </>
